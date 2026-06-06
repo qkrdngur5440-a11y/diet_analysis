f582e8b10e095568485353608a1163d35843f54f
parkwoohyeock <qkrdngur5440@gmail.com>
2026-06-06 12:10:53 +0900

    06060-2

--- src/main/java/com/example/diet_analysis/controller/ExerciseController.java
+++ src/main/java/com/example/diet_analysis/controller/ExerciseController.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
+ 
 @RestController
 @RequestMapping("/api/exercise")
 public class ExerciseController {
